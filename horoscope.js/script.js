dab85e3f44e96504d06420161757b5c1c126819f
Bert-Sipson <163196051+Bert-Sipson@users.noreply.github.com>
2024-07-28 20:29:26 +0100

    Add files via upload

---
+++ horoscope.js/script.js
@@ -0,0 +1,27 @@
+let birthMonth = '--insert month here--'
+
+if (birthMonth == 'January') {
+    console.log('♑  Capricorn')
+} else if (birthMonth == 'February') {
+    console.log('♒  Aquarius')
+} else if (birthMonth == 'March') {
+    console.log('♓	Pisces')
+} else if (birthMonth == 'April') {
+    console.log('♈	Aries')
+} else if (birthMonth == 'May') {
+    console.log('♉	Taurus')
+} else if (birthMonth == 'June') {
+    console.log('♊	Gemini')
+} else if (birthMonth == 'July') {
+    console.log('♋	Cancer')
+} else if (birthMonth == 'August') {
+    console.log('♌	Leo')
+} else if (birthMonth == 'September') {
+    console.log('♍	Virgo')
+} else if (birthMonth == 'October') {
+    console.log('♎	Libra')
+} else if (birthMonth == 'November') {
+    console.log('♏	Scorpio')
+} else if (birthMonth == 'December') {
+    console.log('♐	Sagittarius')
+}
