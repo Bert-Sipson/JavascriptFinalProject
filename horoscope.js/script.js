e29f8f5d9fb8a073609a2e72b00cf778b25c83a1
Bert-Sipson <163196051+Bert-Sipson@users.noreply.github.com>
2024-08-20 13:51:51 +0100

    Add quotes to script.js

--- horoscope.js/script.js
+++ horoscope.js/script.js
@@ -1,27 +1,27 @@
 let birthMonth = '--insert month here--'
 
 if (birthMonth == 'January') {
-    console.log('♑  Capricorn')
+    console.log('♑  This is your day to shine in all your glory, Capricorn.')
 } else if (birthMonth == 'February') {
-    console.log('♒  Aquarius')
+    console.log('♒  Try not to lose sight of your own projects or desires, Aquarius.')
 } else if (birthMonth == 'March') {
-    console.log('♓	Pisces')
+    console.log('♓  Recharge today by getting outside and stretching your arms to the sky, Pisces.')
 } else if (birthMonth == 'April') {
-    console.log('♈	Aries')
+    console.log('♈  Difficult predicaments could arise today if you try to exert your will over others without having the most honorable intentions, Aries.')
 } else if (birthMonth == 'May') {
-    console.log('♉	Taurus')
+    console.log('♉  The fire within you is raging today, Taurus, and you should be careful how you wield this power.')
 } else if (birthMonth == 'June') {
-    console.log('♊	Gemini')
+    console.log('♊  There may be a great deal of fuss over something that seems quite insignificant to you, Gemini.')
 } else if (birthMonth == 'July') {
-    console.log('♋	Cancer')
+    console.log('♋  Things are coming to a critical point for you today, Cancer, and you may find that other people openly object to your actions.')
 } else if (birthMonth == 'August') {
-    console.log('♌	Leo')
+    console.log('♌  Feel free to speak more loudly today, Leo.')
 } else if (birthMonth == 'September') {
-    console.log('♍	Virgo')
+    console.log('♍  You should bask in a glorious splendor today, Virgo.')
 } else if (birthMonth == 'October') {
-    console.log('♎	Libra')
+    console.log('♎  Add more sunshine to your day, Libra.')
 } else if (birthMonth == 'November') {
-    console.log('♏	Scorpio')
+    console.log('♏  You will find that your spark can easily turn potential energy into kinetic energy, Scorpio.')
 } else if (birthMonth == 'December') {
-    console.log('♐	Sagittarius')
+    console.log('♐  Do not dwell on your fluctuating moods today, Sagittarius.')
 }
